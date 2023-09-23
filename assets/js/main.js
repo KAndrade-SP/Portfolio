d18de9d24498182c942699388e4d9a00c3087662
KAndrade-SP <kaique.viana090@gmail.com>
2023-09-22 22:52:11 -0300

    Open and Close menu + Scroll Nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,36 @@ toggleMenu.addEventListener('click', () => {
 })
 
 // HIDDEN
-
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show')
+})
 
 /*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('nav__link')
 
+function linkAction() {
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
